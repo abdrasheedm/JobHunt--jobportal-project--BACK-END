5be36ab725e80e88766ee42f973edf99bcc8523f
abdrasheedm <rasheedvaniyannurrashi175@gmail.com>
2023-01-23 09:17:13 +0530

    Authentication for seeker and recruiter

---
+++ seeker/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from accounts.models import Account
+
+# Create your models here.
+
+
+class SeekerProfile(models.Model):
+    seeker = models.ForeignKey(Account, on_delete=models.CASCADE)
