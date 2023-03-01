a8a16a1276519e90b1d3da7ff9a9264a70d201a9
abdrasheedm <rasheedvaniyannurrashi175@gmail.com>
2023-03-01 21:05:38 +0530

    asgi modifications

--- JobPortal/asgi.py
+++ JobPortal/asgi.py
@@ -10,10 +10,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import django
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JobPortal.settings')
+django.setup()
 
-
-from channels.auth import AuthMiddleware, AuthMiddlewareStack
+from channels.auth import AuthMiddlewareStack
 from .routing import websocket_urlpatterns
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
--- api/models.py
+++ api/models.py
@@ -6,8 +6,8 @@ from django.db import models
 class Me(models.Model):
     name = models.CharField(max_length=20)
 
-class You(models.Model):
-    me = models.ForeignKey(Me, on_delete=models.CASCADE)
-    you = models.CharField(max_length=200)
+
+
+
 
 
